aecaa3a42926f8300c580c5fe5606477ad13ed68
Patrick Steadman <ptsteadman@gmail.com>
2017-12-08 19:45:59 -0500

    add blueprint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'normalize.css'
+import '@blueprintjs/core/dist/blueprint.css'
+import '@blueprintjs/core/resources/icons/icons-20.woff'
+import '@blueprintjs/core/resources/icons/icons-20.ttf'
+import '@blueprintjs/core/resources/icons/icons-20.eot'
+import '@blueprintjs/core/resources/icons/icons.json'
+import 'bootstrap-4-grid/css/grid.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Blockfront</h1>
         </header>
         <p className="App-intro">
