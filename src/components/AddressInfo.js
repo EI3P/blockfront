9980ef039e3e110fa3be155de3bc2d57780d2b23
Robert C Jensen <rob@computerlab.io>
2017-12-29 17:47:11 -0500

    feature: expands txns for a given address

--- src/components/AddressInfo.js
+++ src/components/AddressInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // We want something like `BlockInfoTxn` here for each inbound/outbound txn
 export default class AddressInfo extends React.Component {
@@ -9,8 +10,11 @@ export default class AddressInfo extends React.Component {
       <div>
         <p>Address hash: {addressInfo.id}</p>
         <p>Address balance: {addressInfo.balance}</p>
-        <p>Address transaction count: {addressInfo.txCount}</p>
         <p>Address code: {addressInfo.code}</p>
+        <p>Transactions:</p>
+        <ul>
+          {addressInfo.transactions.map((tx) => <li><Link to={`/tx/${tx.transactionHash}`}>{tx.transactionHash}</Link></li>)}
+        </ul>
       </div>
     );
   }
