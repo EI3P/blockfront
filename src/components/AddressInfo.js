2e0440c04a6234634b3c7eb0f900074c96c41883
Robert C Jensen <rob@computerlab.io>
2017-12-29 18:01:29 -0500

    chore: format address balances in ETH

--- src/components/AddressInfo.js
+++ src/components/AddressInfo.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Web3 from "web3";
 
 // We want something like `BlockInfoTxn` here for each inbound/outbound txn
 export default class AddressInfo extends React.Component {
   render() {
     const { addressInfo } = this.props;
+    const web3 = new Web3();
 
     return (
       <div>
         <p>Address hash: {addressInfo.id}</p>
-        <p>Address balance: {addressInfo.balance}</p>
+        <p>Address balance: {web3.utils.fromWei(addressInfo.balance)} ETH</p>
         <p>Address code: {addressInfo.code}</p>
         <p>Transactions:</p>
         <ul>
