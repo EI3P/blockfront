6edb6ac859c1a3eb726c7e1fc8e219613698fa0a
Robert C Jensen <rob@computerlab.io>
2018-01-02 23:55:10 -0500

    wip: paginate txns in address

--- src/components/Address.js
+++ src/components/Address.js
@@ -14,33 +14,58 @@ class Address extends React.Component {
   constructor() {
     super();
     this.state = {
-      loading: true
+      loading: true,
+      // FIXME: Set from redux
+      // XXX: This should render as `page + 1`
+      page: 0,
+      pageSize: 5
     };
+  }
+
+  getPageOfAddressTxns() {
+    const { addressId } = this.props;
+    const { latestBlockNumber, page, pageSize } = this.state;
+    const { web3 } = config;
+
+    let highBlockNumber = latestBlockNumber - page * pageSize;
+    highBlockNumber = highBlockNumber > 0 ? highBlockNumber : pageSize;
+
+    let lowBlockNumber = highBlockNumber > pageSize ? (highBlockNumber - pageSize) : "0";
+
+    return web3.eth.getPastLogs({
+      "fromBlock": String(lowBlockNumber),
+      "toBlock": String(highBlockNumber),
+      "address": addressId
+    });
   }
 
   componentDidMount() {
     const { addressId } = this.props;
     const { web3 } = config;
 
-    Promise.all([
-      web3.eth.getBalance(addressId, "latest"),
-      web3.eth.getPastLogs({
-        "fromBlock": "0x0",
-        "toBlock": "latest",
-        "address": addressId
-      }),
-      web3.eth.getCode(addressId, "latest"),
-    ]).then(([balance, transactions, code]) => {
-      this.setState({
-        loading: false,
-        addressInfo: {
-          id: addressId,
-          balance,
-          code,
-          transactions,
-        }
+    web3.eth
+      .getBlock("latest", false)
+      .then(block => {
+        this.setState({
+          latestBlockNumber: block.number
+        });
+
+        Promise.all([
+          web3.eth.getBalance(addressId, "latest"),
+          this.getPageOfAddressTxns(),
+          web3.eth.getCode(addressId, "latest"),
+        ]).then(([balance, transactions, code]) => {
+          this.setState({
+            loading: false,
+            addressInfo: {
+              id: addressId,
+              balance,
+              transactions,
+              code,
+            }
+          });
+        });
       });
-    });
   }
 
   render() {
