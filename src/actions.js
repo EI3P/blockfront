afbaf57b36bed36dc04e25b69c9e6f8fbb1823a9
Robert C Jensen <rob@computerlab.io>
2018-01-05 20:41:07 -0500

    wip: redux for address

--- src/actions.js
+++ src/actions.js
@@ -15,6 +15,10 @@ export const RECEIVE_PAGE_OF_BLOCKS = "RECEIVE_PAGE_OF_BLOCKS";
 export const RECEIVE_BLOCK_IN_PAGE = "RECEIVE_BLOCK_IN_PAGE";
 
 // Addresses
+export const REQUEST_ADDRESS = "REQUEST_ADDRESS";
+export const RECEIVE_ADDRESS = "RECEIVE_ADDRESS";
+export const REQUEST_PAGE_OF_ADDRESS_TRANSACTIONS = "REQUEST_PAGE_OF_ADDRESS_TRANSACTIONS";
+export const RECEIVE_PAGE_OF_ADDRESS_TRANSACTIONS = "RECEIVE_PAGE_OF_ADDRESS_TRANSACTIONS";
 export const REQUEST_PAGE_OF_ADDRESSES = "REQUEST_PAGE_OF_ADDRESSES";
 export const RECEIVE_PAGE_OF_ADDRESSES = "RECEIVE_PAGE_OF_ADDRESSES";
 export const RECEIVE_ADDRESS_IN_PAGE = "RECEIVE_ADDRESS_IN_PAGE";
@@ -141,6 +145,70 @@ export function receiveBlockInPage(blockNumber, block) {
     type: RECEIVE_BLOCK_IN_PAGE,
     blockNumber,
     block
+  };
+}
+
+export function fetchAddress(addressId) {
+  return function (dispatch) {
+    dispatch(requestAddress());
+    return Promise.all([
+      web3.eth.getBalance(addressId, "latest"),
+      web3.eth.getCode(addressId, "latest"),
+    ]).then(([balance, code]) => {
+      dispatch(receiveAddress({
+        addressId,
+        balance,
+        code
+      }));
+    });
+  };
+}
+
+export function requestAddress() {
+  return {
+    type: REQUEST_ADDRESS,
+  };
+}
+
+export function receiveAddress(address) {
+  return {
+    type: RECEIVE_ADDRESS,
+    address
+  };
+}
+
+export function fetchPageOfAddressTransactions(addressId, pageNumber) {
+  const PAGE_SIZE = 5;
+
+  return function (dispatch) {
+    dispatch(requestPageOfAddressTransactions());
+
+    return web3.eth.getBlock("latest", false).then(block => {
+      let highBlockNumber = block.number - pageNumber * PAGE_SIZE;
+      highBlockNumber = highBlockNumber > 0 ? highBlockNumber : PAGE_SIZE;
+      let lowBlockNumber = highBlockNumber > PAGE_SIZE ? (highBlockNumber - PAGE_SIZE) : 0;
+
+      return web3.eth.getPastLogs({
+        "fromBlock": String(lowBlockNumber),
+        "toBlock": String(highBlockNumber),
+        "address": addressId
+      }).then((addressTransactions) => {
+        dispatch(receivePageOfAddressTransactions(addressTransactions));
+      });
+    });
+  };
+}
+
+export function requestPageOfAddressTransactions() {
+  return {
+    type: REQUEST_PAGE_OF_ADDRESS_TRANSACTIONS
+  };
+}
+
+export function receivePageOfAddressTransactions(addressTransactions) {
+  return {
+    type: REQUEST_PAGE_OF_ADDRESS_TRANSACTIONS,
+    addressTransactions
   };
 }
 
