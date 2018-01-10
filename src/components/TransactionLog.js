e43c9fb7bb0c4d65036fee829a4235ea37127aa4
Robert C Jensen <robert.cole.jensen@gmail.com>
2018-01-09 20:09:59 -0500

    style nit

--- src/components/TransactionLog.js
+++ src/components/TransactionLog.js
@@ -17,8 +17,7 @@ const TransactionLog = ({ log }) => (
       size="small"
       header={<h3>Topics</h3>}
       dataSource={log.topics}
-      renderItem={(item, index) => (<List.Item><Badge count={index} showZero />&nbsp;<pre>{item}</pre></List.Item>)}
-      split={false}
+      renderItem={(item, index) => (<List.Item><Badge count={index} showZero />&nbsp;<strong>{item}</strong></List.Item>)}
     />
     <div>
       <h3>Data</h3>
